56e8f1791612aa29bd6f437a3247bccd25a2e373
Nika <nikoloz.grigalashvili.1@btu.edu.ge>
2021-11-05 13:37:33 +0400

    First commit

---
+++ app/src/main/java/com/example/nikoloz_grigalashvili_calc/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.nikoloz_grigalashvili_calc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultText: TextView
+
+    private var num1: Double = 0.0
+    private var operation: String = ""
+    private var square: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultText = findViewById(R.id.resultText)
+    }
+
+    fun numberClick(clickedView: View) {
+        if (clickedView is TextView) {
+
+            var result = resultText.text.toString()
+            val number = clickedView.text.toString()
+
+            if (result == "0") {
+                result = "0"
+            }
+
+
+            resultText.text = result + number
+
+        }
+    }
+
+    fun squareClick(clickedView: View) {
+
+        if (clickedView is TextView) {
+
+            val result = resultText.text.toString()
+
+            if (result.isNotEmpty()) {
+                num1 = result.toDouble()
+            }
+
+            square = clickedView.text.toString()
+
+            when(square){
+                "x²" -> resultText.text = (num1 * num1).toString()
+                "x³" -> resultText.text = (num1 * num1 * num1).toString()
+
+            }
+        }
+
+
+
+    }
+
+
+    fun operationClick(clickedView: View) {
+
+        if (clickedView is TextView) {
+
+            val result = resultText.text.toString()
+
+            if (result.isNotEmpty()) {
+                num1 = result.toDouble()
+            }
+
+            operation = clickedView.text.toString()
+
+            resultText.text = ""
+
+        }
+    }
+
+    fun equalsClick(clickedView: View){
+
+        val num2Text: String = resultText.text.toString()
+        var num2: Double = 0.0
+
+        if (num2Text.isNotEmpty()){
+            num2 = num2Text.toDouble()
+        }
+
+        when(operation){
+            "+" -> resultText.text = (num1 + num2).toString()
+            "-" -> resultText.text = (num1 - num2).toString()
+            "*" -> resultText.text = (num1 * num2).toString()
+            "/" -> resultText.text = (num1 / num2).toString()
+
+
+        }
+
+    }
+
+    fun clearClick(clickedView: View){
+        if (clickedView is TextView){
+            resultText.text = ""
+
+        }
+
+    }
+
+}
